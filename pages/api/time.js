6abcbf109b8ef9d351b7ff8747dfd6b994e35d15
jcarvalho-rv <j.carvalho@ruavirtual.pt>
2024-06-14 16:08:39 +0000

    primeira página

--- pages/api/time.js
+++ pages/api/time.js
@@ -1,18 +1,18 @@
-async function Time(request, response) {
+async function apiTime(request, response) {
     const dynamicDate = new Date();
     const apiSecretLang = process.env.FERIADOS_API_LANG;
     const apiFeriados = await fetch(`https://date.nager.at/api/v3/publicholidays/2025/${apiSecretLang}`);
     
     const apiFeriadosJson = await apiFeriados.json();
-    const dataFeriado = apiFeriadosJson[0].date
-    const nomeFeriado = apiFeriadosJson[0].localName
+    const dataFeriado = apiFeriadosJson[0].date;
+    const nomeFeriado = apiFeriadosJson[0].localName;
 
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
     response.json({
         date: dynamicDate.toGMTString(),
         data: dataFeriado,
         nome: nomeFeriado
-    })
+    });
 }
 
-export default Time;
+export default apiTime;
--- pages/time.js
+++ pages/time.js
@@ -1,9 +1,27 @@
+import { useState, useEffect } from 'react';
 import Link from "next/link";
+import apiTime from "./api/time";
 
 function Time(props) {
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
+    
+    // load data from ./api/time
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch('/api/time');
+            const json = await res.json();
+            setData(json);
+        }
 
+        fetchData();
+    }, []);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+    
     return (
         <div>
             <div>
@@ -18,6 +36,13 @@ function Time(props) {
             <div>
                 <div>{dynamicDateString} (dinâmico)</div>
                 <div>{props.staticDateString} (estático)</div>
+            </div>
+            <br></br>
+            <div>
+                <div><b>Resultado da API Time</b></div>
+                <div><b>Data atual:</b> {data.date}</div>
+                <div><b>Data Feriado:</b> {data.data}</div>
+                <div><b>Nome Feriado:</b> {data.nome}</div>
             </div>
         </div>
     )
